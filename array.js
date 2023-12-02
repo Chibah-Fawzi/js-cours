44536a2b3954c1c9d86df246fb6f17c364cebfe5
fawzi <fawzichibah@gmail.com>
2023-12-02 22:58:23 +0100

    first

---
+++ array.js
@@ -0,0 +1,14 @@
+var ma_liste = ["Omar", "said", "hmed", "salima"];
+
+ma_liste[2] = "Houria"; // Changer un élement de la liste
+
+ma_liste.push("Sofiane"); // Ajouter un élement à la fin de la liste
+ma_liste.unshift("Mokhtar"); // Ajouter un élement au début de la liste
+
+ma_liste.pop(); // Retirer un élement à la fin de la liste
+ma_liste.shift(); // Retirer un élement au début de la liste
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb"); // Permet d'ajouter ou de retirer des élements au milieu
+
+console.log(ma_liste);
---
+++ script.js
@@ -0,0 +1,107 @@
+// alert("Hello !");
+// confirm("Are you sure ?");
+// prompt("hello");
+
+var a = 10; // TYPE NUMBER
+// var b = "10"; // TYPE STRING
+var b = 50;
+var c = 150;
+
+// // Arithmétique
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a ** b);
+// console.log(a % b);
+
+// var func = (a + b / 4) ** 4;
+
+// console.log(func);
+
+// a = a + 30;
+
+// console.log(a);
+
+// // == : Compare les valeurs
+// // === : Compare les valeurs et les type des variables
+// console.log(a === b);
+
+// // LOGIQUE COMBINATOIRE
+// console.log(a > b && c > b);
+// console.log(a > b || c > b);
+
+// // CONDITIONS
+// if (a > c) {
+//   console.log("Le a est plus petit que c");
+// } else {
+//   console.log("Le a n'est pas plus grand que c");
+// }
+
+// Script Manége
+// var age = 10;
+// var taille = 1.55;
+// var accompagné = true;
+
+// if ((age >= 12 && taille >= 1.6 && taille <= 2.1) || accompagné) {
+//   console.log("Va jouer");
+// } else {
+//   console.log("tu ne peux pas jouer");
+// }
+
+// var color = "orange";
+
+// switch (color) {
+//   case "red":
+//     console.log("La couleur est rouge");
+//     break;
+//   case "violet":
+//     console.log("La couleur est violet");
+//     break;
+//   case "yellow":
+//     console.log("La couleur est yellow");
+//     break;
+//   case "bleu":
+//     console.log("La couleur est bleu");
+//     break;
+//   case "green":
+//     console.log("La couleur est green");
+//     break;
+
+//   default:
+//     console.log("Couleure inconnue");
+//     break;
+// }
+
+// // LOOPS
+// var i = 0;
+// while (i < 5) {
+//   console.log("Hello");
+//   i++;
+// }
+
+// do {
+//   console.log("Je suis une itération");
+// } while (i < 0);
+
+// for (let index = 0; index < 100; index++) {
+//   console.log("Je suis une itération");
+// }
+
+// // FUNCTIONS
+
+function Bonjour() {
+  alert("Bonjour à vous");
+}
+
+function f(x) {
+  console.log(x + 10);
+}
+
+f(10);
+
+function Hello(nom, age) {
+  console.log("Hello " + nom + " âgé de " + age + " ans");
+}
+
+Hello("Said", 40);
